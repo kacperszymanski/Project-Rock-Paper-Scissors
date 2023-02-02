41b74f022e42486a626fde27f9db0910f27d7c73
Kacper <akacperszymanski@gmail.com>
2023-02-02 15:44:11 +0100

    add UI, changes in css html and app

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
 let playerSelection = '';
 let playerPoints = 0;
 let computerPoints = 0;
-
 const boxWithPlayerChoice = document.getElementById('boxWithPlayerChoice');
 
-boxWithPlayerChoice.addEventListener('click', event => {
+boxWithPlayerChoice.addEventListener('click', (event) => {
   if (event.target.nodeName !== 'BUTTON') return;
 
   playerSelection = event.target.id.replace('playerChoice', '').toLowerCase();
-  document.getElementById("WhatPlayerChooses").innerHTML = `Player chooses ${playerSelection}`;
+  document.getElementById("WhatPlayerChooses").innerHTML = playerSelection;
   game(playerSelection);
 });
 
-function playRound(playerSelection) {
-  let movesList = ["rock", "paper", "scissors"];
+const movesList = ["rock", "paper", "scissors"];
+
+const playRound = (playerSelection) => {
   let computerSelection = movesList[Math.floor(Math.random() * movesList.length)];
-    document.getElementById("WhatComputerChooses").innerHTML = `Computer chooses ${computerSelection}`;
+  document.getElementById("WhatComputerChooses").innerHTML = computerSelection;
   if (playerSelection === computerSelection) return 0;
   if ((playerSelection === "rock" && computerSelection === "scissors") ||
       (playerSelection === "scissors" && computerSelection === "paper") ||
       (playerSelection === "paper" && computerSelection === "rock")) return 1;
   return 2;
-}
+};
 
-function game(playerSelection) {
+const game = (playerSelection) => {
   let verdict = playRound(playerSelection);
-
-  if (verdict === 0) {
-    document.getElementById("whoWinRound").innerHTML = "Draw";
-  } else if (verdict === 1) {
-    document.getElementById("whoWinRound").innerHTML = "Player wins";
+  let winMessage;
+  if (verdict === 0) winMessage = "Draw";
+  else if (verdict === 1) {
+    winMessage = "Player wins";
     playerPoints++;
   } else {
-    document.getElementById("whoWinRound").innerHTML = "Computer wins";
+    winMessage = "Computer wins";
     computerPoints++;
   }
-
+  document.getElementById("whoWinRound").innerHTML = winMessage;
   document.getElementById("gameResult").innerHTML = `Player points ${playerPoints} - Computer points ${computerPoints}`;
-}
+};
